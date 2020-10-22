aa76fd85770232a5556ec68dc746b338cd6fa78c
fajaryoedha <fajaryoedha@gmail.com>
2020-10-22 13:13:12 +0700

    new theme

--- myassets/getbio.js
+++ myassets/getbio.js
@@ -7,26 +7,52 @@ fetch('myassets/bio_en.json')
         document.getElementById("bio_moto").innerHTML = data.moto
         document.getElementById("bio_birth").innerHTML = data.birth
         document.getElementById("bio_address").innerHTML = data.address
-        document.getElementById("bio_nationaly").innerHTML = data.nationaly
+        document.getElementById("bio_nationality").innerHTML = data.nationality
+        document.getElementById("bio_phone").innerHTML = data.phone
+        // document.getElementById("bio_whatsapp").innerHTML = data.whatsapp
+        document.getElementById("bio_email").innerHTML = data.email
+        var educationsStr = ""
         data.educations.map((education, index) => {
-            document.getElementById("edu_year_" + index).innerHTML = education.year
-            document.getElementById("edu_name_" + index).innerHTML = education.name
-            document.getElementById("edu_study_" + index).innerHTML = education.study
-        })
-        var skills = ""
-        data.skills.map((skill, index) => {
-            console.log(skill.name)
-            skills += `<li class="list-group-item">
-            <div class="row">
-                <div class="col-2">`+ skill.name + `</div>
-                <div class="col">
-                    <div class="progress" style="height: 2vh;">
-                        <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: `+skill.value+`%"></div>
+            if (index % 2) {
+                educationsStr += `<li class="timeline-inverted">`
+            }
+            else {
+                educationsStr += `<li>`
+            }
+            educationsStr += `
+                <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/1.jpg"
+                    alt="" /></div>
+                <div class="timeline-panel">
+                    <div class="timeline-heading">
+                        <h4>` + education.year + `</h4>
+                        <h4 class="subheading">`+ education.name + `</h4>
+                    </div>
+                    <div class="timeline-body">
+                        <h5 class="text-muted">`+ education.study + `</h5>
                     </div>
                 </div>
-            </div>
-        </li>`
+            </li>`
         })
-        document.getElementById("bio_skills").innerHTML = skills
+        educationsStr += `
+            <li class="timeline-inverted">
+                <div class="timeline-image">
+                    <h4>
+                        <br/>
+                        Still learn
+                        <br />
+                        till die!
+                    </h4>
+                </div>
+            </li>
+        `
+        document.getElementById("bio_educations").innerHTML = educationsStr
+        var skillsStr = ""
+        data.skills.map((skill, index) => {
+            let head = Math.floor(Math.random() * 6) + 1
+            skillsStr += `
+                <h`+ head + ` style="display: inline;">` + skill + `, </h` + head + `>
+            `
+        })
+        document.getElementById("bio_skills").innerHTML = skillsStr
 
     });
