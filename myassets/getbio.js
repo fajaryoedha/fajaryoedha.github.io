c36ba755852810d3414d1f4951c6b6d12f0607c4
fajaryoedha <fajaryoedha@gmail.com>
2020-10-23 16:37:44 +0700

    fix untill educations

--- myassets/getbio.js
+++ myassets/getbio.js
@@ -20,7 +20,7 @@ fetch('myassets/bio_en.json')
                 educationsStr += `<li>`
             }
             educationsStr += `
-                <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/1.jpg"
+                <div class="timeline-image"><img class="rounded-circle img-fluid" src="`+education.img+`"
                     alt="" /></div>
                 <div class="timeline-panel">
                     <div class="timeline-heading">
@@ -54,5 +54,64 @@ fetch('myassets/bio_en.json')
             `
         })
         document.getElementById("bio_skills").innerHTML = skillsStr
-
+        var projectsStr = ""
+        data.projects.map((project, index) => {
+            console.log(project.year)
+            var skillUsedStr = ""
+            project.skills.map(skill => {
+                skillUsedStr += skill + ', '
+            })
+            projectsStr += `
+                <div class="col-lg-4 col-sm-6 mb-4">
+                    <!-- thumbnail -->
+                    <div class="portfolio-item">
+                        <a class="portfolio-link" data-toggle="modal" href="#portfolioModal`+index+`">
+                            <div class="portfolio-hover">
+                                <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                            </div>
+                            <img class="img-fluid img-thumbnail" src="`+project.img[0]+`" alt="" />
+                        </a>
+                        <div class="portfolio-caption">
+                            <div class="portfolio-caption-heading">`+project.name+`</div>
+                            <div class="portfolio-caption-subheading text-muted">`+project.year+`</div>
+                        </div>
+                    </div>
+                    <!-- Modal -->
+                    <div class="portfolio-modal modal fade" id="portfolioModal`+index+`" tabindex="-1" role="dialog"
+                        aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="close-modal" data-dismiss="modal"><img src="assets/img/close-icon.svg"
+                                        alt="Close modal" />
+                                </div>
+                                <div class="container">
+                                    <div class="row justify-content-center">
+                                        <div class="col-lg-8">
+                                            <div class="modal-body">
+                                                <!-- Project Details Go Here-->
+                                                <h2 class="text-uppercase">`+project.name+`</h2>
+                                                </p>
+                                                <img class="img-fluid d-block mx-auto" src="`+project.img[1]+`" alt="" />
+                                                <p>
+                                                    `+project.description+`
+                                                </p>
+                                                <ul class="list-inline">
+                                                    <li>Year : `+project.year+`</li>
+                                                    <li>Used Skill : `+skillUsedStr+`</li>
+                                                </ul>
+                                                <button class="btn btn-primary" data-dismiss="modal" type="button">
+                                                    <i class="fas fa-times mr-1"></i>
+                                                    Close Project
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `
+        })
+        document.getElementById("bio_projects").innerHTML = projectsStr
     });
